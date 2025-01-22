ac6f71ec1abe57ee6192cac098839fda66799b73
SoyQuillo <sjquinteros@ufpso.edu.co>
2025-01-21 22:14:18 -0500

    Ejercicio matches cambiar color

--- eventos/index.js
+++ eventos/index.js
@@ -1,22 +1,16 @@
-const wrapper = document.querySelector('[data-id="wrapper"]');
-const input = document.querySelector('[type = "text"]');
-const changeUser = document.querySelector('[data-id="name"]');
 const section = document.querySelector(".section");
-const buttonShow = document.querySelector('[data-id="button-show"]');
-const buttonHide = document.querySelector('[data-id="button-hide"]');
 
-const elements = document.querySelectorAll("[data-id]");
 
-console.log(elements);
+function eventDelegation(e){
+    if(e.target.matches(".button-color")){
+        const getColor = e.target.getAttribute('data-color');
+        
+         e.currentTarget.style.backgroundColor = getColor
 
-for(element of elements){
-    const currentElement = element.matches('[data-id = "button-show"]');
-
-    if(currentElement){
-        console.log(element)
-        console.log(`Elemento ${element.nodeName} contiene el valor buttonShow en su atributo`);
     }
-
 }
 
-console.log(input.closest('.wrapper'));
+section.addEventListener('click', eventDelegation)
+
+
+
