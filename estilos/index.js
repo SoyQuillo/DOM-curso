935140bf8e4851c36e31f4dc06968908f808d1cb
SoyQuillo <sjquinteros@ufpso.edu.co>
2025-01-22 16:44:35 -0500

    ejercicio cambiar color y cambiar tamaño

--- estilos/index.js
+++ estilos/index.js
@@ -1,12 +1,16 @@
 const title = document.querySelector(".title");
 const inputColor = document.querySelector('[type="color"]');
-const inputRange = document.querySelector('[type="range]');
+const inputRange = document.querySelector('[type="range"]');
 
-document.documentElement.style.setProperty('--color--title', 'hotpink');
+inputColor.addEventListener('change', function(e){
+    const newColor = e.target.value;
+    
+    title.style.color = newColor;
+})
 
-title.style.fontFamily = "cursive";
-title.style.color = "var(--color--title)";
-
-title.style.removeProperty("font-family");
-title.style.color = "";
+inputRange.addEventListener('change', function(e){
+    const newRange = e.target.value;
+    title.style.fontSize = newRange +'px'
+    title.style.setProperty('font-size', newRange+'px');
+})
 
