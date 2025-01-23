c500e267f309d470ded8032814cf7a443a5f3df7
SoyQuillo <sjquinteros@ufpso.edu.co>
2025-01-23 14:22:44 -0500

    append prepend before after

--- todo-list/index.js
+++ todo-list/index.js
@@ -5,6 +5,14 @@ const listContainer = document.querySelector('.list-container');
 
  form.addEventListener('submit', handleSubmit);
 
+ function message(){
+   const h3 = document.createElement("h3");
+
+   h3.textContent = listContainer.firstElementChild ? 'Tareas por hacer' : 'No hay tareas aun';
+
+   listContainer.before(h3);
+ }
+
  function handleSubmit(e){
     e.preventDefault();
 
@@ -22,7 +30,8 @@ const listContainer = document.querySelector('.list-container');
     const newTask = document.createElement("li");
     newTask.textContent = value;
 
-    listContainer.appendChild(newTask);
+    listContainer.append(newTask);
     
  }
 
+ message();
